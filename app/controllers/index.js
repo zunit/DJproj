05a14d372b19bde4b1e60ea90193b9d9f3967be1
zunit <jackjiananzhou@gmail.com>
2015-03-17 11:32:48 -0400

    added unlocking

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -8,6 +8,9 @@ var win = Titanium.UI.createWindow({
 // creating a view
 var view = Titanium.UI.createView();
 
+//this is how many points you have.
+var mypoints = 100;
+
 var custom_data = [
 	// {leftImage: 'http://www.ginaraimondo.com/sites/ginaraimondo/files/bill-clinton.jpg', title:'Car1', hasChild:true},
 	// {leftImage: '/images/default.png', title:'Jooo', haschild:true}
@@ -38,43 +41,39 @@ var custom_data = [
 		seen:true, 
 		hasChild:true,
 		points: '-1'
+	},
+	{
+		profile_picture:'/images/stela.jpg', 
+		name:"John", 
+		age:'20', 
+		hasChild:true,
+		points: '80'
+	},
+	{
+		profile_picture:'/images/stela.jpg', 
+		name:"John", 
+		age:'20', 
+		hasChild:true,
+		points: '100'
+	},
+	{
+		profile_picture:'/images/stela.jpg', 
+		name:"Sandy", 
+		age:'22', 
+		hasChild:true,
+		points: '200'
 	}
 ];
 
 // we will push all the customized data into this list after reformatting
 var data = [];
 
-// looping through each custom_data to create the right view. 
-for (i = 0; i < custom_data.length; i++){
-	
-	// creating the row
-	var row = Ti.UI.createTableViewRow();
-	
-	// picture flag
-	var profile_picture = Ti.UI.createImageView({
-		image: custom_data[i].profile_picture,
-		height: 48,
-		width: 48,
-		left: 4,
-		top: 2
-	});
-	
+function messagedisplay(data_load){
 	// seen value 
-	if (custom_data[i].seen) {
-		// seen create name 
-		var name = Ti.UI.createLabel({
-			text: custom_data[i].name,
-			font:{fontSize:16},
-			width: 'auto',
-			textAlign:'left',
-			top: 2, 
-			left: 55,
-			height: 16
-		});
-		
+	if (data_load.seen) {		
 		// seen last message
-		var last_message = Ti.UI.createLabel({
-			text: custom_data[i].last_message,
+		return Ti.UI.createLabel({
+			text: data_load.last_message,
 			font:{fontSize:12},
 			textAlign: 'left',
 			width: 'auto',
@@ -84,20 +83,9 @@ for (i = 0; i < custom_data.length; i++){
 		});
 		
 	} else {
-		// create name 
-		var name = Ti.UI.createLabel({
-			text: custom_data[i].name,
-			font:{fontSize:17, fontWeight:'bold'},
-			width: 'auto',
-			textAlign:'left',
-			top: 2, 
-			left: 55,
-			height: 16
-		});
-		
 		// last message
-		var last_message = Ti.UI.createLabel({
-			text: custom_data[i].last_message,
+		return Ti.UI.createLabel({
+			text: data_load.last_message,
 			font:{fontSize:13, fontWeight:'bold'},
 			textAlign: 'left',
 			width: 'auto',
@@ -105,34 +93,115 @@ for (i = 0; i < custom_data.length; i++){
 			bottom: 5,
 			height: 12
 		});	
+	}	
+}
+
+function namedisplay(namedata){
+	if (namedata.seen) {
+		// seen create name 
+		return Ti.UI.createLabel({
+			text: namedata.name,
+			font:{fontSize:16},
+			width: 'auto',
+			textAlign:'left',
+			top: 2, 
+			left: 55,
+			height: 16
+		});
+	} else {
+		// create name 
+		return Ti.UI.createLabel({
+			text: namedata.name,
+			font:{fontSize:17, fontWeight:'bold'},
+			width: 'auto',
+			textAlign:'left',
+			top: 2, 
+			left: 55,
+			height: 16
+		});			
+	}
+}
+
+function profiledisplay(data_load){
+	return Ti.UI.createImageView({
+		image: data_load.profile_picture,
+		height: 48,
+		width: 48,
+		left: 4,
+		top: 2
+	});
+}
+
+// function for figuring out valid point or not
+function calculate_points(points){
+	if (mypoints >= points){
+		alert("Success you have" + String(mypoints - points) + " left!");
+	} else {
+		alert("not enough points! you need " + String(points - mypoints));
+	}
+}
+
+// looping through each custom_data to create the right view. 
+for (i = 0; i < custom_data.length; i++){
+	
+	// creating the row
+	var row = Ti.UI.createTableViewRow();
+	var profile_picture = profiledisplay(custom_data[i]);
+	var name = namedisplay(custom_data[i]);
+	
+	// if we can talk to the user
+	if (custom_data[i].points == -1){
+		var last_message = messagedisplay(custom_data[i]);	
+		row.add(last_message);
+	} else {
+		// have to unlock with points 
+		// case 1 enough points 
+		// this is a button 
+		
+		// this creates the label
+		var unlock = Ti.UI.createLabel({
+			text: "Unlock " + custom_data[i].points + " points",
+			font:{fontSize:17, fontWeight:'bold'},
+			width: 'auto',
+			textAlign:'right',
+			top: 2, 
+			right: 55,
+			height: 16
+		});		
+		row.add(unlock);
+		// case 2 not enough points 		
 	}
 	
-	 
+	
 	row.height = 60;
 	row.add(profile_picture);
 	row.add(name);
-	row.add(last_message);
+	row.points = custom_data[i].points;
+	
 	row.hasChild = custom_data[i].hasChild;
 	row.className = 'profile_tabs';
-	
+	console.log("row data: " + row.points);
 	data.push(row);
 }
 
 // creating a Label
 var table = Titanium.UI.createTableView({
 	data: data
-});
+});  
 
-// function showMessage(event) {
-    // var showMessage = event.source;
-    // console.log("MESSAGEPAGE" + showMessage);
-    // var args = {
-        // title: "HELLO",
-        // //author: selectedBook.author
-    // };
-    // var messageView = Alloy.createController("messaging_page", args).getView();
-    // messageView.open();
-// }   
+// click button
+table.addEventListener('click', function(e) {
+	if (e.rowData.points == -1){
+		alert("you received a message!");
+	}
+	else if (e.rowData.points <= mypoints){
+		var selectedPerson = e.rowData;
+	    alert("Unlocked! go say hi!");
+	    alert("You have " + (mypoints - e.rowData.points) + " left!");
+	} else {
+    	alert("not enough points! You need " + (e.rowData.points - mypoints) + " points!");
+    }
+});
 
 view.add(table);
 win.add(view);
